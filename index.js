1e5765da927475db644fb48a01aa31f173b56d97
saipaul@hotmail.com <saipaul@hotmail.com>
2018-08-29 02:21:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var temp = []; var i;
+  for (i = 0; i<musicians.length; i++) {
+    var stat = musicians[i] + ` plays ` + instruments[i]
+    temp[i] = stat
+  }
+  return temp
+}
